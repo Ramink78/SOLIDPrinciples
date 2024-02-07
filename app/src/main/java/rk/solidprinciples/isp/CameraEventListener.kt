6b730ffdef59af89e926341cf01a47c6b499b485
Ramin Karimi <raminkarimi1378@gmail.com>
2024-02-07 17:39:37 +0330

    solve the ISP violation

--- app/src/main/java/rk/solidprinciples/isp/CameraEventListener.kt
+++ app/src/main/java/rk/solidprinciples/isp/CameraEventListener.kt
@@ -1,33 +1,25 @@
 package rk.solidprinciples.isp
 
-// Violating ISP
-interface CameraEventListener {
+interface ImageCaptureListener {
     fun onTakenPictureListener()
+    fun onCameraModeChangeListener()
+
+}
+
+interface VideoCaptureListener {
     fun onRecordVideoListener()
     fun onFrameRateChanged()
     fun onVideoResolutionChanged()
-    fun onCameraModeChangeListener()
 }
 
-class ImageCapture : CameraEventListener {
+class ImageCapture : ImageCaptureListener {
     override fun onTakenPictureListener() {
         TODO("Not yet implemented")
-    }
-
-    override fun onRecordVideoListener() {
-        // It doesn't need to expose.
-    }
-
-    override fun onFrameRateChanged() {
-        // It doesn't need to expose.
-    }
-
-    override fun onVideoResolutionChanged() {
-        // It doesn't need to expose.
     }
 
     override fun onCameraModeChangeListener() {
         TODO("Not yet implemented")
     }
+
 }
 
